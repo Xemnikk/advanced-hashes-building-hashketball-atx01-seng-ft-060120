b1d6571f2e492d71c4fcaa5a8abfaaa0214c5b8e
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-09 18:21:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,8 +2,8 @@
 def game_hash
   hash = {
     :home => {
-      :team_name =>
-      :colors =>
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
       :players => [
         {
         :player_name => "Alan Anderson",
