e40e08719ea0b5f7cf6144ae9d56e0e1262e5e5a
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-09 18:01:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -47,7 +47,7 @@ def game_hash
         :assists => 6, 
         :steals => 3, 
         :blocks => 8, 
-        :slam_dunks => 5, 
+        :slam_dunks => 5
       },
           {
         :player_name => "Jason Terry",
