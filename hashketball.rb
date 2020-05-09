38be3f355491d760ecf30b7a787de69726204685
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-09 18:13:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,20 +1,7 @@
 # Write your code here!
 def game_hash
-  {
-    
-    :home => {
-      :team_name => "Brooklyn Nets",
-      :colors => ["Black", "White"],
-      :players => ["l","a"]
-    }
-    },
-    {
-      
-    :away => {
-      :team_name => "Charlotte Hornets",
-      :colors => ["Turquoise", "Purple"],
-       :players => ["l","a"]
-    
-    }
-    }
+  hash = {
+    :home => l,
+    :away => a,
+  }
   
